d1a556dec1be4a132acfb53479e30f081b2ba1f4
Marcos Evaldt <marcos.evaldt@uhuu.com>
2020-08-19 17:23:55 -0300

    Ajustando cluster

--- producer.js
+++ producer.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const open = require('amqplib').connect('amqp://rabbit');
+const open = require('amqplib').connect('amqp://admin:admin@rabbit1');
 const queue = 'tasks';
 
 
