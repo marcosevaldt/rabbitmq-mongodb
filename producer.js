65f3d5ece7497995191eeeed7e7f53268065de2c
Mickelangelo Pohren <mickelangelo.ponhren@gmail.com>
2020-08-20 12:08:26 -0300

    Create data schema

--- producer.js
+++ producer.js
@@ -1,23 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 const app = express()
 const open = require('amqplib').connect('amqp://admin:admin@haproxy:5672');
-const queue = 'tasks';
+const queue = 'tasks'
 
 
-app.post('/produce', function (req, res) {
+app.post('/produce', jsonParser, function (req, res) {
   open.then(function(conn) {
     return conn.createChannel();
   }).then(function(ch) {
     return ch.assertQueue(queue, { durable: true }).then(function(ok) {
-      let msg = 'something';
-      let date = new Date();
-      ch.sendToQueue(queue, Buffer.from(msg), { persistent: true });
-      console.log(" [x] '%s' Sent '%s'", date, msg);
+      const msg = req.body;
+
+      const msgString = JSON.stringify(msg);
+
+      ch.sendToQueue(queue, Buffer.from(msgString), { persistent: true });
+
+      console.log(" [x] '%s' Sent '%s'", msgString);
+
       return ch.close();
     });
   }).catch(console.warn);
 
   res.send({});
 })
- 
+
 app.listen(process.env.NODE_PORT);
